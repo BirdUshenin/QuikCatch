d28e2882f228c45ae84a339bb73d9877581d58f6
Ilya <ilyaushenin@gmail.com>
2023-11-23 21:46:54 +0300

    Initial commit

---
+++ app/src/main/java/com/birdushenin/quikcatch/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.birdushenin.quikcatch.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.birdushenin.quikcatch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
